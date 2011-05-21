8e515410a365f64894a4238b539a1d0157da3824
jack.chen <jack.chen@isobar.com>
2011-05-21 22:39:19 +0800

    project initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stellar::Application.config.secret_token = 'aa408f41c6f53381021df56ea967395ba825ec5b7229438a6d4005a508143b986f0033cbf8189dafa977947de36c5dfd58bd2cf0ea4e9e1a8c6d6abc20ccd494'
